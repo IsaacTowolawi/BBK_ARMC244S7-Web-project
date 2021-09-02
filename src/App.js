0d9b3a2800ed0599e47749eaaa66da5b953f257b
IsaacTowolawi <79358417+IsaacTowolawi@users.noreply.github.com>
2021-09-02 10:22:19 +0100

    fourth  commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {BrowserRouter as Router, Switch, Route, BrowserRouter} from "react-router-dom"
+import {HashRouter as Router, Switch, Route, BrowserRouter} from "react-router-dom"
 import { useState} from 'react'
 import { Header } from "./components/Header"
 import { AnimeBase } from './components/AnimeBase'
